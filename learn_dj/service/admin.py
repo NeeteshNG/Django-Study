9f6569d2b727111715377deb2bd9fc874e3ff768
Neetesh NG <neeteshng15@gmail.com>
2023-12-08 13:16:18 +0530

    Made Some Important Chnages for Admin.

--- learn_dj/service/admin.py
+++ learn_dj/service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from service.models import Service
 
 # Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    list_display=('service_name', 'service_title', 'service_desc')
+
+admin.site.register(Service, ServiceAdmin)
