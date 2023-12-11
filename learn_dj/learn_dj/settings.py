1ce4dfdbe25ad0b06be12f19a5c624a99401d1b2
Neetesh NG <neeteshng15@gmail.com>
2023-12-11 12:54:56 +0530

    Setup for Media Uploadation.

--- learn_dj/learn_dj/settings.py
+++ learn_dj/learn_dj/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR, "static"
 ]
+
+MEDIA_ROOT = BASE_DIR /"media"
+
+MEDIA_URL = "/media"
--- learn_dj/learn_dj/urls.py
+++ learn_dj/learn_dj/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from learn_dj import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +34,6 @@ urlpatterns = [
     path('course/<str:coursestr>', views.courseDetailsStr),
     path('course/<slug:courseslug>', views.courseDetailsSlug)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
